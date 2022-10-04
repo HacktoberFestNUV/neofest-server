eaacb266bf6ddcf0e830855e149f9eb3cc3eba22
OhmPatil <workpatil10@gmail.com>
2022-10-04 13:44:26 +0530

    Create procfile

--- server.js
+++ server.js
@@ -19,19 +19,18 @@ app.listen(3001, async () => {
 });
 
 
-import nodeoutlook from 'nodejs-nodemailer-outlook'
-nodeoutlook.sendEmail({
-  auth: {
-    user: "",
-    pass: '',
-  },
-  from: "20124034@nuv.ac.in",
-  to: "20124041@nuv.ac.in",
-  subject: "WOOOOO",
-  html: "<b>This is bold text</b>",
-  text: "This is text version!",
-  replyTo: "receiverXXX@gmail.com",
-  
-  onError: (e) => console.log(e),
-  onSuccess: (i) => console.log(i),
-});
+// import nodeoutlook from 'nodejs-nodemailer-outlook'
+// nodeoutlook.sendEmail({
+//   auth: {
+//     user: "",
+//     pass: '',
+//   },
+//   from: "20124034@nuv.ac.in",
+//   to: "20124041@nuv.ac.in",
+//   subject: "WOOOOO",
+//   html: "<b>This is bold text</b>",
+//   text: "This is text version!",
+//   replyTo: "receiverXXX@gmail.com",
+//   onError: (e) => console.log(e),
+//   onSuccess: (i) => console.log(i),
+// });
